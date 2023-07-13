2c39eed05ea1d16add47dec55af9e486b9d3a2af
xjf7711 <xjf7711@qq.com>
2023-07-13 21:34:11 +0800

    export function

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { TypeNode } from './type-node/type-node.abstract';
 export { ITypeNode, INodeAttr, IPath } from './type-node/type-node.interface';
 export { TypeElement } from './type-element/type-element.abstract';
 export { ITypeElement, ITypeAttribute, ITypeProperty } from './type-element/type-element.interface';
+export { toJSON, pxToRem, getScroll, humpToMiddleLine } from './type-element/type-element.function';
 export { TextNode } from './text-node/text-node.class';
 export { ITextNode } from './text-node/text-node.interface';
 export { IStyle } from './style/style.interface';
