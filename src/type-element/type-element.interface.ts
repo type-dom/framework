a03fd463f5d0248fa22e374ae756ee1b1c36b2f1
xjf7711 <xjf7711@qq.com>
2023-08-02 10:04:05 +0800

    TypeClass

--- src/type-element/type-element.interface.ts
+++ src/type-element/type-element.interface.ts
@@ -4,6 +4,52 @@
 import { ITypeNode } from '../type-node/type-node.interface';
 import { IStyle } from '../style/style.interface';
 import {TypeElement} from "./type-element.abstract";
+import {
+  A,
+  Area,
+  Article,
+  Aside,
+  Audio,
+  B,
+  Br,
+  Button,
+  Canvas,
+  Code,
+  Division,
+  Form,
+  Header,
+  Heading,
+  Hr,
+  I,
+  Img,
+  Input,
+  Label,
+  ListItem,
+  Main,
+  Map,
+  Menu,
+  Option,
+  P,
+  Picture,
+  Pre,
+  Progress,
+  Section,
+  Select,
+  Slot,
+  Span, SvgCircle, SvgEllipse, SvgImage, SvgLine, SvgPath, SvgRect, SvgSvg, SvgText,
+  Table,
+  TableBody,
+  TableDataCell,
+  TableFoot,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
+  Template,
+  Textarea,
+  Title,
+  Track, UnorderedList, Video
+} from "../element";
+import {template} from "@babel/core";
 export interface ITypeAttribute {
   // id?: string;
   class?: string,
@@ -41,5 +87,22 @@ export interface IXItem extends ITypeNode {
   // methods: Record<string, Function>
 }
 export interface IElementItem extends ITypeNode {
-  TypeClass: TypeElement,
+  TypeClass: typeof A | typeof Area | typeof Article | typeof Aside | typeof Audio
+    | typeof B | typeof Br | typeof Button
+    | typeof Canvas | typeof Code
+    | typeof Division
+    | typeof Form
+    | typeof Header | typeof Heading | typeof Hr
+    | typeof I | typeof Img | typeof Input
+    | typeof Label | typeof ListItem
+    | typeof Main | typeof Map | typeof Menu
+    | typeof Option
+    | typeof P | typeof Picture | typeof Pre | typeof Progress
+    | typeof Section | typeof Select | typeof Slot | typeof Span
+    | typeof Table | typeof TableRow | typeof TableBody | typeof TableFoot | typeof TableHead | typeof TableDataCell | typeof TableHeaderCell
+    | typeof Template | typeof Textarea | typeof Title | typeof Track
+    | typeof UnorderedList
+    | typeof Video
+    | typeof SvgCircle | typeof SvgEllipse | typeof SvgImage | typeof SvgLine | typeof SvgPath | typeof SvgRect | typeof SvgSvg | typeof SvgText
+    | any
 }
