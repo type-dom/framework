e527f11cb085cf61295b3c19346be4ab7abf7b61
xjf7711 <xjf7711@qq.com>
2023-08-17 21:40:45 +0800

    ellipse propObj

--- src/element/svg-element/ellipse/ellipse.class.ts
+++ src/element/svg-element/ellipse/ellipse.class.ts
@@ -5,31 +5,33 @@ export class SvgEllipse extends TypeSvg implements ISvgEllipse {
   nodeName: 'ellipse';
   className: 'SvgEllipse';
   dom: SVGEllipseElement;
-  // propObj: ISvgEllipseProperty;
+  propObj: ISvgEllipseProperty;
   childNodes: [];
-  cx: number | string = 0;
-  cy: number | string = 0;
-  rx: number | string = 0;
-  ry: number | string = 0;
-
+  cx: number = 0;
+  cy: number = 0;
+  rx: number = 0;
+  ry: number = 0;
   constructor(public parent: TypeSvg) {
     super('ellipse');
     this.nodeName = 'ellipse';
     this.className = 'SvgEllipse';
     this.dom = document.createElementNS('http://www.w3.org/2000/svg', this.nodeName);
     this.childNodes = [];
-    this.addAttrObj({
-      fill: 'none',
-      stroke: '#000',
-      strokeWidth: 1,
-      cx: this.cx,
-      cy: this.cy,
-      rx: this.rx,
-      ry: this.ry
-    });
+    this.propObj = {
+      styleObj: {},
+      attrObj: {
+        fill: 'none',
+        stroke: '#000',
+        strokeWidth: 1,
+        cx: this.cx,
+        cy: this.cy,
+        rx: this.rx,
+        ry: this.ry
+      }
+    }
   }
 
-  reset(cx: string | number, cy: string | number, rx: string | number, ry: string | number): SvgEllipse {
+  reset(cx: number, cy: number, rx: number, ry: number): SvgEllipse {
     this.cx = cx;
     this.cy = cy;
     this.rx = rx;
