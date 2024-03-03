304338f9527d8374bc7f2210c5e58278474789fa
xjf7711 <xjf7711@qq.com>
2024-03-03 21:14:02 +0800

    test: add test file

--- src/router/router.spec.ts
+++ src/router/router.spec.ts
@@ -1,8 +1,18 @@
 import { Router } from './router.class';
-
+/**
+ * @jest-environment window
+ * @jest-environment jsdom
+ * @jest-environment ./router.class.ts
+ */
 describe('router', () => {
   it('should work', () => {
+    console.log('router.spec.ts should work . ');
     const router = new Router({ routes: [] });
+    // console.log('router is ', router);
     expect(router.routes.length === 0).toEqual(true);
   });
+  // test('use jsdom in this test file', () => {
+  //   const element = document.createElement('div');
+  //   expect(element).not.toBeNull();
+  // });
 });
