bc266bf6ab584ccd705893c30536bf6635b2d4a3
xjf7711 <xjf7711@qq.com>
2024-03-17 23:20:40 +0800

    chore: x-element

--- src/lib/element/x-element/x-element.interface.ts
+++ src/lib/element/x-element/x-element.interface.ts
@@ -1,11 +1,7 @@
-import type {
-  ITypeElement,
-  ITypeProperty
-} from '../../type-element/type-element.interface';
+import type { ITypeElement } from '../../type-element/type-element.interface';
 import type { ITextNode } from '../../text-node/text-node.interface';
 
 export interface IXElement extends ITypeElement {
   className: 'XElement';
   childNodes: (IXElement | ITextNode)[];
-  propObj: ITypeProperty;
 }
