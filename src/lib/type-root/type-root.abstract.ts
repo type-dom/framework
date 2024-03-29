74deb9559491cd939742515329ac045714a6889b
jeffrey xu <jianfeng.xu@cssca.com>
2024-03-29 08:55:14 +0800

    chore: type-root

--- src/lib/type-root/type-root.abstract.ts
+++ src/lib/type-root/type-root.abstract.ts
@@ -1,13 +1,14 @@
-/**
- * TypeRoot是一个元素根节点抽象类
- * 作为前端项目的入口文件要继承根节点抽象类，并挂载到对应的 ID 上。
- */
 import { TypeHtml } from '../type-element/type-html/type-html.abstract';
 import type { ITypeRoot, ITypeRootConfig } from './type-root.interface';
 
 /**
- * el 元素对象或ID；
- * parent 只有自己 TypeRoot
+ * TypeRoot是一个元素根节点抽象类
+ * 作为前端项目的入口文件要继承根节点抽象类，并挂载到对应的html页面元素上。
+ * 挂载使用mount方法。
+ * 根节点可以挂载其他子节点，也可以挂载其他根节点。
+ * 根节点挂载到页面上后，其他子节点会自动挂载到根节点上。
+ * 根节点抽象类
+ * isRoot: true
  */
 export abstract class TypeRoot extends TypeHtml implements ITypeRoot {
   nodeName: string;
