224192c0ffda6884da215d18523c1fbdea75e2c3
jeffrey xu <jianfeng.xu@cssca.com>
2024-04-12 09:11:30 +0800

    chore: x-element;

--- src/lib/element/x-element/x-element.class.ts
+++ src/lib/element/x-element/x-element.class.ts
@@ -1,8 +1,8 @@
 import { fromEvent } from 'rxjs';
-import type { IAttr } from '../../type-node/type-node.interface';
-import { TextNode } from '../../text-node/text-node.class';
 import { Parser } from '../../parser/parser.class';
 import { TypeElement } from '../../type-element/type-element.abstract';
+import type { IAttr } from '../../type-node/type-node.interface';
+import { TextNode } from '../../text-node/text-node.class';
 import { IXElement } from './x-element.interface';
 // import { ITypeConfig } from '../../config.interface';
 
