4713d525630222dcae185df70138513a97e96320
jeffrey xu <jianfeng.xu@cssca.com>
2024-04-30 09:03:17 +0800

    chore: add parser

---
+++ src/parser/index.ts
@@ -0,0 +1,2 @@
+export { Parser } from './parser.class';
+export type * from './parser.interface';
---
+++ src/parser/parser.interface.ts
@@ -0,0 +1,25 @@
+import type { IAttr } from '../core/type-node/type-node.interface';
+
+export interface IContent {
+  name: string;
+  attributes: IAttr[]; // ToDo 为什么用数组方式，而不是键值对？？？？
+  parsed: number;
+}
+
+// 指令接口
+export interface IInstruction {
+  name: string;
+  value: string;
+  parsed: number;
+}
+
+export interface IComponent {
+  pos: number;
+  name: string;
+}
+
+// 解析器参数
+export interface IParserParam {
+  hasAttributes?: boolean;
+  lowerCaseName?: boolean;
+}
