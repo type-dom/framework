a3f7a13b12e91eb633f114aab577a6111fa4d5fe
jeffrey xu <jianfeng.xu@cssca.com>
2024-05-13 09:30:11 +0800

    chore: modify html-element

--- src/core/element/html-element/section/section.class.ts
+++ src/core/element/html-element/section/section.class.ts
@@ -1,6 +1,6 @@
 import { ITypeConfig } from '../../../type-node/type-node.interface';
 import { TypeSection } from '../../../type-element/type-html/section/section.abstract';
-import { StyleDisplay } from '../../../style/style.enum';
+import { StyleDisplay } from '../../../../style/style.enum';
 import type { ISection } from './section.interface';
 
 export class Section extends TypeSection implements ISection {
