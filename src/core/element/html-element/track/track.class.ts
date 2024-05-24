859c1f3b8099a9a4b0ebc7b0586cc88dc72a3a5e
jeffrey xu <jianfeng.xu@cssca.com>
2024-05-24 09:15:32 +0800

    chore: track

--- src/core/element/html-element/track/track.class.ts
+++ src/core/element/html-element/track/track.class.ts
@@ -1,4 +1,4 @@
-import { ITypeConfig } from '../../../type-node/type-node.interface';
+import type { ITypeConfig } from '../../../type-node/type-node.interface';
 import { TypeTrack } from '../../../type-element/type-html/track/track.abstract';
 import type { ITrack } from './track.interface';
 
