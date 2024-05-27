53ba3f1d3ef31370895229911ae319113519a9b8
jeffrey xu <jianfeng.xu@cssca.com>
2024-05-27 09:48:02 +0800

    chore: title tag

--- src/core/element/html-element/title/title.class.ts
+++ src/core/element/html-element/title/title.class.ts
@@ -1,4 +1,4 @@
-import { ITypeConfig } from '../../../type-node/type-node.interface';
+import type { ITypeConfig } from '../../../type-node/type-node.interface';
 import { TypeTitle } from '../../../type-element/type-html/title/title.abstract';
 import type { ITitle } from './title.interface';
 
