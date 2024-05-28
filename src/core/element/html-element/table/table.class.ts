65d1ea43852c5976054090656e0ba80c38f79e4d
jeffrey xu <jianfeng.xu@cssca.com>
2024-05-28 09:13:23 +0800

    chore: optimize table

--- src/core/element/html-element/table/table.class.ts
+++ src/core/element/html-element/table/table.class.ts
@@ -1,5 +1,5 @@
 import { TypeTable } from '../../../type-element/type-html/table/table.abstract';
-import { ITypeConfig } from '../../../type-node/type-node.interface';
+import type { ITypeConfig } from '../../../type-node/type-node.interface';
 import { TableRow } from './row/row.class';
 import { TableHead } from './head/head.class';
 import type { ITable } from './table.interface';
