2fcc1a54b50f45969cfd54236d7f03628c8babab
jeffrey xu <jianfeng.xu@cssca.com>
2024-07-01 09:24:59 +0800

    chore: optimize interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,8 +1,16 @@
 import { Observer } from './observer/observer';
 import { XProxy } from './observer/x-proxy/x-proxy.class';
 
-export type IPrimitive = null | undefined | string | number | boolean | symbol | bigint;
+export type IPrimitive =
+  | null
+  | undefined
+  | string
+  | number
+  | boolean
+  | symbol
+  | bigint;
 type IBrowserNativeObject = Date | FileList | File | Blob | RegExp;
+
 /**
  * 一个通用的json数据结构的接口
  * 定义一个接口 IJsonConfig，它是一个键值对的集合，其中键是字符串类型，值是 IJsonProp 类型。
@@ -21,10 +29,15 @@ export interface IJsonData {
  * - undefined
  * 这个类型用于表示 JSON 对象中的属性值，可以是简单的数据类型，也可以是嵌套的 JSON 对象或数组。
  */
-export type IJsonDataProp = IPrimitive | XProxy<IJsonData> | IJsonData | IJsonDataProp[];
+export type IJsonDataProp =
+  | IPrimitive
+  | XProxy<IJsonData>
+  | IJsonData
+  | IJsonDataProp[];
 
 export interface IObData {
-  '__ob__'?: Observer;
+  __ob__?: Observer;
+
   [propName: string]: IObDataProp;
 }
 
@@ -32,4 +45,4 @@ export type IObDataProp = IPrimitive | Observer | IObData | IObData[];
 
 // If the type T accepts type "any", output type Y, otherwise output type N.
 // https://stackoverflow.com/questions/49927523/disallow-call-with-any/49928360#49928360
-export type IfAny<T, Y, N> = 0 extends 1 & T ? Y : N
+export type IfAny<T, Y, N> = 0 extends 1 & T ? Y : N;
