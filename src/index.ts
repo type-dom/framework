5fa30f34bfd44ed87a97b4b8a2ac2f61c3cba1ee
jeffrey xu <jianfeng.xu@cssca.com>
2024-07-02 09:51:17 +0800

    chore: optimize index

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,13 @@
-export * from './shared/util';
 export * from './interface';
 export * from './hooks/index';
+export * from './style/index';
+export * from './parser';
+export * from './router';
+export * from './observer/index';
+export * from './reactivity/index';
+export * from './util/index';
 export { TypeNode } from './core/type-node/type-node.abstract';
-export type {
-  ITypeNode,
-  IAttr,
-  IPath,
-  ITypeConfig,
-  IOptionSet,
-  ISettings,
-  IOptionSetting,
-  IOptionConfig,
-} from './core/type-node/type-node.interface';
-export type { IStyle } from './style/style.interface';
-export * from './style/style.enum';
+export type * from './core/type-node/type-node.interface';
 export { XNode } from './core/x-node/x-node.class';
 export type { IXNode } from './core/x-node/x-node.interface';
 export { TextNode } from './core/text-node/text-node.class';
@@ -22,10 +16,3 @@ export { TypeRoot } from './core/type-root/type-root.abstract';
 export type * from './core/type-root/type-root.interface';
 export * from './core/type-element';
 export * from './core/element';
-export * from './parser';
-export * from './router';
-export * from './observer/index';
-export * from './reactivity/index';
-export * from './util/index';
-
-
