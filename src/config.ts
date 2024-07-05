11e5ddd77c44883c5ad71db37d2cea5ce54aab7d
jeffrey xu <jianfeng.xu@cssca.com>
2024-07-05 10:00:43 +0800

    chore: optimize config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // import { no, noop, identity } from '../shared/util';
-import { LIFECYCLE_HOOKS } from './constants'
+import { LIFECYCLE_HOOKS } from './constants';
 import { TypeNode as Component } from './core/type-node/type-node.abstract';
 
 /**
@@ -8,28 +8,28 @@ import { TypeNode as Component } from './core/type-node/type-node.abstract';
 export interface Config {
   // user
   // optionMergeStrategies: { [key: string]: Function }
-  silent: boolean
-  productionTip: boolean
-  performance: boolean
-  devtools: boolean
-  errorHandler?: (err: Error, vm: Component | null, info: string) => void
-  warnHandler?: (msg: string, vm: Component | null, trace: string) => void
-  ignoredElements: Array<string | RegExp>
-  keyCodes: { [key: string]: number | Array<number> }
+  silent: boolean;
+  productionTip: boolean;
+  performance: boolean;
+  devtools: boolean;
+  errorHandler?: (err: Error, vm: Component | null, info: string) => void;
+  warnHandler?: (msg: string, vm: Component | null, trace: string) => void;
+  ignoredElements: Array<string | RegExp>;
+  keyCodes: { [key: string]: number | Array<number> };
 
   // platform
-  isReservedTag: (x: string) => boolean | undefined
-  isReservedAttr: (x: string) => true | undefined
-  parsePlatformTagName: (x: string) => string
-  isUnknownElement: (x: string) => boolean
-  getTagNamespace: (x: string) => string | undefined
-  mustUseProp: (tag: string, type?: string | null, name?: string) => boolean
+  isReservedTag: (x: string) => boolean | undefined;
+  isReservedAttr: (x: string) => true | undefined;
+  parsePlatformTagName: (x: string) => string;
+  isUnknownElement: (x: string) => boolean;
+  getTagNamespace: (x: string) => string | undefined;
+  mustUseProp: (tag: string, type?: string | null, name?: string) => boolean;
 
   // private
-  async: boolean
+  async: boolean;
 
   // legacy
-  _lifecycleHooks: Array<string>
+  _lifecycleHooks: Array<string>;
 }
 
 export const config = {
@@ -124,6 +124,6 @@ export const config = {
    * Exposed for legacy reasons
    */
   _lifecycleHooks: LIFECYCLE_HOOKS
-} as unknown as Config
+} as unknown as Config;
 
 export const __DEV__ = true;
