c74912e57b6ff2be36dd8bcdae8793b40e39f2c8
jeffrey xu <jianfeng.xu@cssca.com>
2024-07-15 09:27:40 +0800

    chore: svg element defs

--- src/core/element/svg-element/defs/defs.class.ts
+++ src/core/element/svg-element/defs/defs.class.ts
@@ -1,24 +1,31 @@
+/**
+ * `SvgDefs` 类定义了SVG定义元素的封装，用于创建和管理SVG定义（如渐变、过滤器等）。
+ * 它继承自 `TypeSvg` 类，实现了 `ISvgDefs` 接口。
+ */
 import { TypeSvg } from '../../../type-element/type-svg/type-svg.abstract';
 import { ITypeConfig } from '../../../type-node/type-node.interface';
 import type { ISvgDefs } from './defs.interface';
 
-// todo 有文字内容的
 export class SvgDefs extends TypeSvg implements ISvgDefs {
-  nodeName: 'defs';
-  className: 'SvgDefs';
-  dom: SVGDefsElement;
-  override childNodes: TypeSvg[];
+  nodeName: 'defs'; // SVG元素的节点名称
+  className: 'SvgDefs'; // 类名，用于CSS选择器
+  dom: SVGDefsElement; // SVG DOM元素的引用
+  override childNodes: TypeSvg[]; // 存储子节点的数组
 
-  constructor(config?: Partial<ITypeConfig>) {
-    super();
-    this.nodeName = 'defs';
-    this.className = 'SvgDefs';
+  /**
+   * 构造函数初始化 `SvgDefs` 实例。
+   * @param {ITypeConfig} config 可选的配置对象，用于配置SVG定义元素。
+   */
+  constructor(config?: ITypeConfig) {
+    super(); // 调用父类的构造函数
+    this.nodeName = 'defs'; // 设置节点名称
+    this.className = 'SvgDefs'; // 设置类名
+    // 创建SVG defs元素，并设置为当前实例的DOM引用
     this.dom = document.createElementNS(
       'http://www.w3.org/2000/svg',
       this.nodeName
     );
-    // this.textNode = new WebTextNode(this, '字');
-    this.childNodes = [];
-    this.setConfig(config);
+    this.childNodes = []; // 初始化子节点数组
+    this.setConfig(config); // 设置传入的配置项
   }
 }
