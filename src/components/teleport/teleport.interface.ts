07d068f0449ef55ec2dfdd0f1cc3744c49922c45
jeffrey xu <jianfeng.xu@cssca.com>
2024-07-22 09:09:12 +0800

    chore: teleport

---
+++ src/components/teleport/teleport.interface.ts
@@ -0,0 +1,13 @@
+import {
+  ITypeConfig,
+  ITypeNode,
+} from '../../core/type-node/type-node.interface';
+
+export interface ITeleport extends ITypeNode {
+  className: 'Teleport';
+}
+
+export interface ITeleportConfig extends ITypeConfig {
+  // to?: string | TypeElement;
+  disabled?: boolean;
+}
