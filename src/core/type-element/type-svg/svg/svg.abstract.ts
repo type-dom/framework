32cf40fe0541b6f67bdb0dfdeb2866c99b063df5
jeffrey xu <jianfeng.xu@cssca.com>
2024-07-26 09:51:15 +0800

    chore: type-svg/svg

--- src/core/type-element/type-svg/svg/svg.abstract.ts
+++ src/core/type-element/type-svg/svg/svg.abstract.ts
@@ -19,7 +19,7 @@ export abstract class TypeSvgSvg extends TypeSvg implements ITypeSvgSvg {
       version: '1.1',
       xmlns: 'http://www.w3.org/2000/svg',
       width: this.width,
-      height: this.height
+      height: this.height,
       // viewBox: '0 0 1024 1024'
     });
     this.childNodes = [];
@@ -28,7 +28,7 @@ export abstract class TypeSvgSvg extends TypeSvg implements ITypeSvgSvg {
   resetSize(width: string | number, height: string | number): void {
     this.setAttrObj({
       width,
-      height
+      height,
     });
   }
 }
