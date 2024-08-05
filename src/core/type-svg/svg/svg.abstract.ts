477a84fd219a3dfc2ba514ada7ed8ae82ec9d27b
jeffrey xu <jianfeng.xu@cssca.com>
2024-08-05 09:24:24 +0800

    chore: type-svg change folder

---
+++ src/core/type-svg/svg/svg.abstract.ts
@@ -0,0 +1,32 @@
+import { TypeElement } from '../../type-element/type-element.abstract';
+import { TypeSvg } from '../type-svg.abstract';
+import type { ITypeSvgSvg } from './svg.interface';
+
+export abstract class TypeSvgSvg extends TypeSvg implements ITypeSvgSvg {
+  nodeName: 'svg';
+  dom: SVGSVGElement;
+  override childNodes: TypeSvg[];
+  width: string | number = '100%';
+  height: string | number = '100%';
+
+  protected constructor() {
+    super();
+    this.nodeName = 'svg';
+    this.dom = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    this.addAttrObj({
+      version: '1.1',
+      xmlns: 'http://www.w3.org/2000/svg',
+      width: this.width,
+      height: this.height,
+      // viewBox: '0 0 1024 1024'
+    });
+    this.childNodes = [];
+  }
+
+  resetSize(width: string | number, height: string | number): void {
+    this.setAttrObj({
+      width,
+      height,
+    });
+  }
+}
