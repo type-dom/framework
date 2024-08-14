f94904ae1b675261b3c69b174dacce61327f2dd8
jeffrey xu <jianfeng.xu@cssca.com>
2024-08-14 10:29:35 +0800

    chore: svg-element linear-gradient

--- src/core/element/svg-element/linear-gradient/linear-gradient.class.ts
+++ src/core/element/svg-element/linear-gradient/linear-gradient.class.ts
@@ -1,4 +1,4 @@
-import { TypeSvg } from '../../../type-element/type-svg/type-svg.abstract';
+import { TypeSvg } from '../../../type-svg/type-svg.abstract';
 import { SvgStop } from '../stop/stop.class';
 import type {
   ISvgLinearGradient,
