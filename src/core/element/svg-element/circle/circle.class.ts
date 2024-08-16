3f5d554005f0364b775476a0258b84e4cf99b6cd
jeffrey xu <jianfeng.xu@cssca.com>
2024-08-16 09:30:30 +0800

    chore: svg-element  circle

--- src/core/element/svg-element/circle/circle.class.ts
+++ src/core/element/svg-element/circle/circle.class.ts
@@ -1,5 +1,5 @@
-import { TypeSvg } from '../../../type-element/type-svg/type-svg.abstract';
-import { TypeSvgSvg } from '../../../type-element/type-svg/svg/svg.abstract';
+import { TypeSvg } from '../../../type-svg/type-svg.abstract';
+import { TypeSvgSvg } from '../../../type-svg/svg/svg.abstract';
 import { ITypeConfig } from '../../../type-node/type-node.interface';
 import type { ISvgCircle } from './circle.interface';
 
