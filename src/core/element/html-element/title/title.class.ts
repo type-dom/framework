e8c6abb536df7b2617713d8797b72bc7dd544c9b
jeffrey xu <jianfeng.xu@cssca.com>
2024-08-20 09:27:53 +0800

    chore: html-element title

--- src/core/element/html-element/title/title.class.ts
+++ src/core/element/html-element/title/title.class.ts
@@ -1,5 +1,5 @@
 import type { ITypeConfig } from '../../../type-node/type-node.interface';
-import { TypeTitle } from '../../../type-element/type-html/title/title.abstract';
+import { TypeTitle } from '../../../type-html/title/title.abstract';
 import type { ITitle } from './title.interface';
 
 export class Title extends TypeTitle implements ITitle {
