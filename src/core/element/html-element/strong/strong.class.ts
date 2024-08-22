b70b5ca38f56bb41bdfb0729be04ffea3c99a249
jeffrey xu <jianfeng.xu@cssca.com>
2024-08-22 10:12:21 +0800

    chore: html-element strong

--- src/core/element/html-element/strong/strong.class.ts
+++ src/core/element/html-element/strong/strong.class.ts
@@ -1,5 +1,5 @@
 import type { ITypeConfig } from '../../../type-node/type-node.interface';
-import { TypeStrong } from '../../../type-element/type-html/strong/strong.abstract';
+import { TypeStrong } from '../../../type-html/strong/strong.abstract';
 import type { IStrong } from './strong.interface';
 
 export class Strong extends TypeStrong implements IStrong {
