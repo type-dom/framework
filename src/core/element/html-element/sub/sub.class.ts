f1128d5c5c9e28345107aacb82deefc9e7204b5a
jeffrey xu <jianfeng.xu@cssca.com>
2024-08-22 10:12:07 +0800

    chore: html-element sub

--- src/core/element/html-element/sub/sub.class.ts
+++ src/core/element/html-element/sub/sub.class.ts
@@ -1,5 +1,5 @@
 import type { ITypeConfig } from '../../../type-node/type-node.interface';
-import { TypeSub } from '../../../type-element/type-html/sub/sub.abstract';
+import { TypeSub } from '../../../type-html/sub/sub.abstract';
 import type { ISub } from './sub.interface';
 
 export class Sub extends TypeSub implements ISub {
