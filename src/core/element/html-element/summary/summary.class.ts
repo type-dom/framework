6a95bf88c2e5faac92185c05fa55b60196c356c3
jeffrey xu <jianfeng.xu@cssca.com>
2024-08-22 10:11:55 +0800

    chore: html-element summary

--- src/core/element/html-element/summary/summary.class.ts
+++ src/core/element/html-element/summary/summary.class.ts
@@ -1,5 +1,5 @@
 import type { ITypeConfig } from '../../../type-node/type-node.interface';
-import { TypeSummary } from '../../../type-element/type-html/summary/summary.abstract';
+import { TypeSummary } from '../../../type-html/summary/summary.abstract';
 import type { ISummary } from './summary.interface';
 
 export class Summary extends TypeSummary implements ISummary {
