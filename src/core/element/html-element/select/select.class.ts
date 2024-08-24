c44e6bc5499eca569f52f0e41397bfdfebee1b3b
jeffrey xu <jianfeng.xu@cssca.com>
2024-08-24 11:21:11 +0800

    chore: html-element select

--- src/core/element/html-element/select/select.class.ts
+++ src/core/element/html-element/select/select.class.ts
@@ -1,5 +1,5 @@
 import type { ITypeConfig } from '../../../type-node/type-node.interface';
-import { TypeSelect } from '../../../type-element/type-html/select/select.abstract';
+import { TypeSelect } from '../../../type-html/select/select.abstract';
 import { Option } from '../option/option.class';
 import type { ISelect } from './select.interface';
 
--- src/core/element/html-element/select/select.interface.ts
+++ src/core/element/html-element/select/select.interface.ts
@@ -1,4 +1,4 @@
-import type { ITypeSelect } from '../../../type-element/type-html/select/select.interface';
+import type { ITypeSelect } from '../../../type-html/select/select.interface';
 import type { IOption } from '../option/option.interface';
 
 export interface ISelect extends ITypeSelect {
