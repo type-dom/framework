228b060be1390ebe48c02c92eb0fb5c7e2a96b59
jeffrey xu <jianfeng.xu@cssca.com>
2024-08-27 09:40:08 +0800

    chore: html-element ruby

--- src/core/element/html-element/ruby/ruby.class.ts
+++ src/core/element/html-element/ruby/ruby.class.ts
@@ -1,5 +1,5 @@
 import type { ITypeConfig } from '../../../type-node/type-node.interface';
-import { TypeRuby } from '../../../type-element/type-html/ruby/ruby.abstract';
+import { TypeRuby } from '../../../type-html/ruby/ruby.abstract';
 import type { IRuby } from './ruby.interface';
 
 export class Ruby extends TypeRuby implements IRuby {
