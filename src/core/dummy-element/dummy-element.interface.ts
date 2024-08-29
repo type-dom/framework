f1ad1bfd3f50bc78e9914f26b6150f774614a769
jeffrey xu <jianfeng.xu@cssca.com>
2024-08-29 09:39:25 +0800

    chore: dummy-element

--- src/core/dummy-element/dummy-element.interface.ts
+++
@@ -1,7 +0,0 @@
-import { ITypeNode } from '../type-node/type-node.interface';
-
-export interface IDummyElement extends ITypeNode {
-  nodeName: undefined;
-  nodeValue: undefined;
-  dom: undefined;
-}
