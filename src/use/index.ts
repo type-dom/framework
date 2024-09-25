39291a32c6e15732ef67bc98160635fde67d8c6a
jeffrey xu <jianfeng.xu@cssca.com>
2024-09-25 10:07:59 +0800

    chore: add use

--- src/use/index.ts
+++ src/use/index.ts
@@ -1,8 +1,6 @@
 export * from './toValue';
 export * from './use-timeout-fn';
-export { useCursor } from './use-cursor';
 export { useElementBounding } from './use-element-bounding';
 export { useWindowSize } from './use-window-size';
 export { useSameTarget, type IUseSameTargetReturn } from './use-same-target';
-export { useDraggable } from './use-draggable'
-export { useEscapeKeydown } from './use-escape-keydown';
+export { useDraggable } from './use-draggable';
--- src/use/use-same-target.ts
+++ src/use/use-same-target.ts
@@ -1,9 +1,9 @@
 import { NOOP } from '../constants';
 
 export interface IUseSameTargetReturn {
-  onClick: (e: MouseEvent) => void,
-  onMousedown: (e: MouseEvent) => void,
-  onMouseup: (e: MouseEvent) => void
+  onClick: (e?: MouseEvent) => void,
+  onMousedown: (e?: MouseEvent) => void,
+  onMouseup: (e?: MouseEvent) => void
 }
 
 /**
@@ -15,7 +15,7 @@ export interface IUseSameTargetReturn {
  * @param handleClick 可选参数，一个鼠标点击事件的处理函数如果没有提供，钩子将返回一组空操作函数
  * @returns 返回一个对象，包含三个事件处理函数 onClick, onMousedown, onMouseup
  */
-export function useSameTarget(handleClick?: (e: MouseEvent) => void): IUseSameTargetReturn {
+export function useSameTarget(handleClick?: (e?: MouseEvent) => void): IUseSameTargetReturn {
   // 如果没有提供 handleClick 函数，返回一组空操作函数
   if (!handleClick) {
     return { onClick: NOOP, onMousedown: NOOP, onMouseup: NOOP };
@@ -31,7 +31,7 @@ export function useSameTarget(handleClick?: (e: MouseEvent) => void): IUseSameTa
    * 并重置 mousedownTarget 和 mouseupTarget 标记
    * @param e MouseEvent 对象
    */
-  const onClick = (e: MouseEvent) => {
+  const onClick = (e?: MouseEvent) => {
     if (mousedownTarget && mouseupTarget) {
       handleClick(e);
     }
@@ -43,8 +43,8 @@ export function useSameTarget(handleClick?: (e: MouseEvent) => void): IUseSameTa
    * 标记当前 mousedown 事件的目标是否与当前目标相同
    * @param e MouseEvent 对象
    */
-  const onMousedown = (e: MouseEvent) => {
-    mousedownTarget = e.target === e.currentTarget;
+  const onMousedown = (e?: MouseEvent) => {
+    mousedownTarget = e?.target === e?.currentTarget;
   };
 
   /**
@@ -52,10 +52,10 @@ export function useSameTarget(handleClick?: (e: MouseEvent) => void): IUseSameTa
    * 标记当前 mouseup 事件的目标是否与当前目标相同
    * @param e MouseEvent 对象
    */
-  const onMouseup = (e: MouseEvent) => {
-    mouseupTarget = e.target === e.currentTarget;
+  const onMouseup = (e?: MouseEvent) => {
+    mouseupTarget = e?.target === e?.currentTarget;
   };
 
   // 返回一组用于处理 mousedown, mouseup 和 click 事件的函数
   return { onClick, onMousedown, onMouseup };
-};
+}
