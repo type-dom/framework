1e43d7639f032bf893ecff710180885bd4beb631
jeffrey xu <jianfeng.xu@cssca.com>
2024-10-08 10:15:34 +0800

    chore: type-html

--- src/core/type-html/type-html.abstract.ts
+++ src/core/type-html/type-html.abstract.ts
@@ -4,6 +4,7 @@
  */
 import { TypeElement } from '../../core/type-element/type-element.abstract';
 import type { ITypeHtml } from './type-html.interface';
+import type { ITypeConfig } from '../type-node/type-node.interface';
 
 export abstract class TypeHtml extends TypeElement implements ITypeHtml {
   abstract override nodeName: string; // 必然有；
@@ -13,4 +14,12 @@ export abstract class TypeHtml extends TypeElement implements ITypeHtml {
    * 子类需要实现这个属性，以提供对具体HTML元素的访问。
    */
   abstract override dom: HTMLElement;
+
+  override useParams<T extends ITypeConfig>(params = {} as T): T {
+    // 插槽默认替换子节点；
+    if (params.slot) {
+      this.slotChild(params.slot);
+    }
+    return super.useParams(params);
+  }
 }
