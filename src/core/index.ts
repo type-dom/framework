ed29e214c849fbf672258dba43e2243006dd61b8
jeffrey xu <jianfeng.xu@cssca.com>
2024-10-23 09:32:32 +0800

    chore: core index

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,3 +3,4 @@ export type { ITypeFragment } from './type-fragment/type-fragment.interface';
 export { vHash, TypeElement } from './type-element/type-element.abstract';
 export type * from './type-element/type-element.interface';
 export * from './style/index';
+export type * from './event-emitter/event-emitter.interface';
