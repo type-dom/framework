8cce3bd50782c2dc65c0349390d2727f4b6af5a3
jeffrey xu <jianfeng.xu@cssca.com>
2024-11-11 09:37:39 +0800

    chore: type-fragment

--- src/core/type-fragment/type-fragment.abstract.ts
+++ src/core/type-fragment/type-fragment.abstract.ts
@@ -1,13 +1,27 @@
 import { TypeElement } from '../type-element/type-element.abstract';
 import { ITypeFragment } from './type-fragment.interface';
+import { SlotNode } from '../../components';
 
 export abstract class TypeFragment extends TypeElement implements ITypeFragment {
-  nodeName: 'fragment';
-  dom: undefined;
+  override nodeName: 'fragment';
+  override dom: DocumentFragment;
+  style: undefined;
+  attr: undefined;
 
   constructor() {
     super();
     this.nodeName = 'fragment';
-    this.dom = undefined;
+    this.dom = document.createDocumentFragment();
   }
+
+  /**
+   * 确保slot存在，不存在则创建；
+   * 要在useSlots之后调用
+   * teleport.class.ts:2 Uncaught ReferenceError: Cannot access 'TypeFragment' before initialization
+   * @param name
+   */
+  // getSlotNode(name = 'default') {
+  //   // return this.slotNodes[name];
+  //   return this.slotNodes[name] = this.slotNodes[name] ?? new SlotNode(name);
+  // }
 }
