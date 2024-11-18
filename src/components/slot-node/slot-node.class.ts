759a7c076ef3ef00172ec2c8b464cd1c3ada21a6
jeffrey xu <jianfeng.xu@cssca.com>
2024-11-18 09:35:26 +0800

    chore: slot-node

--- src/components/slot-node/slot-node.class.ts
+++ src/components/slot-node/slot-node.class.ts
@@ -1,14 +1,15 @@
 import { TypeFragment } from '../../core/type-fragment/type-fragment.abstract';
-// import { TypeComponent } from '../../core/type-component/type-component.abstract';
 import { TypeNode } from '../../core/type-node/type-node.abstract';
+import { TextNode } from '../../core/text-node/text-node.class';
 import { ISlotNodeConfig } from './slot-node.interface';
 
 export class SlotNode extends TypeFragment {
   className: 'SlotNode';
   name: string;
   override props: ISlotNodeConfig;
+  override childNodes: TypeNode[];
 
-  constructor(name: string, slot?: string | TypeNode | (string | TypeNode)[]) {
+  constructor(name = 'default', slot?: string | TypeNode | (string | TypeNode)[]) {
     super();
     this.className = 'SlotNode';
     this.params = {
@@ -16,11 +17,12 @@ export class SlotNode extends TypeFragment {
       slot,
     };
     this.name = name;
+    this.childNodes = [];
     if (slot) {
       // this.resetSlot(slot);
       this.slotChild(slot);
     }
-    this.props = this.buildProps({
+    this.props = this.assignProps({
       name,
       slot,
     });
@@ -35,8 +37,23 @@ export class SlotNode extends TypeFragment {
     this.slotChild(slot);
   }
 
+  // todo 要避免修改 slot 的parent，
+  //    避免和直接使用 slotChild 冲突
   resetSlot(slot?: string | TypeNode | (string | TypeNode)[]) {
     this.clearChildren(); // 要清空插槽
-    this.slotChild(slot);
+    // this.slotChild(slot);
+    if (typeof slot === 'string') {
+      this.childNodes = [new TextNode(slot)];
+    } else if (slot instanceof TypeNode) {
+      this.childNodes = [slot];
+    } else if (Array.isArray(slot)) {
+      for (const item of slot) {
+        if (typeof item === 'string') {
+          this.childNodes.push(new TextNode(item));
+        } else if (item instanceof TypeNode) {
+          this.childNodes.push(item);
+        }
+      }
+    }
   }
 }
