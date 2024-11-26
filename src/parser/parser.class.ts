b1acd29ed69e9874268e8d6d432c822790bc9eb7
jeffrey xu <jianfeng.xu@cssca.com>
2024-11-26 09:38:54 +0800

    chore: parser

--- src/parser/parser.class.ts
+++ src/parser/parser.class.ts
@@ -1,7 +1,7 @@
 import {
   XMLParserErrorCode,
   isWhitespace,
-  isWhitespaceString,
+  isWhitespaceString
 } from '@type-dom/utils';
 import { XElement } from '../index'; // todo type-element.abstract 中有引用。
 // import { XElement } from "../element/x-element/x-element.class"; // todo 这样会报错。上面引入没问题；
@@ -49,7 +49,7 @@ export class Parser {
         case 'quot':
           return '"';
         case 'apos':
-          return "'";
+          return '\'';
       }
       return this.onResolveEntity(entity);
     });
@@ -76,7 +76,7 @@ export class Parser {
       !isWhitespace(s, pos) &&
       s[pos] !== '>' &&
       s[pos] !== '/'
-    ) {
+      ) {
       ++pos;
     }
     const name = s.substring(start, pos);
@@ -86,7 +86,7 @@ export class Parser {
       s[pos] !== '>' &&
       s[pos] !== '/' &&
       s[pos] !== '?'
-    ) {
+      ) {
       skipWs();
       let attrName = '';
       let attrValue = '';
@@ -102,7 +102,7 @@ export class Parser {
       skipWs();
       const attrEndChar = s[pos];
       // 限定了属性值必须以 ' or " 结尾 ， 不能注掉，会解析出错
-      if (attrEndChar !== '"' && attrEndChar !== "'") {
+      if (attrEndChar !== '"' && attrEndChar !== '\'') {
         // console.log(`attrEndChar !== '"' && attrEndChar !== "'"`);
         return null;
       }
@@ -121,7 +121,7 @@ export class Parser {
       // } else {
       attributes.push({
         name: attrName,
-        value: value,
+        value: value
       });
       // }
       pos = attrEndIndex + 1;
@@ -130,7 +130,7 @@ export class Parser {
     return {
       name,
       attributes,
-      parsed: pos - start,
+      parsed: pos - start
     };
   }
 
@@ -149,7 +149,7 @@ export class Parser {
       s[pos] !== '>' &&
       s[pos] !== '?' &&
       s[pos] !== '/'
-    ) {
+      ) {
       ++pos;
     }
     const name = s.substring(start, pos);
@@ -162,7 +162,7 @@ export class Parser {
     return {
       name,
       value,
-      parsed: pos - start,
+      parsed: pos - start
     };
   }
 
