70f7c23ebf02a2f2a2c1f16a24d492013e382dda
jeffrey xu <jianfeng.xu@cssca.com>
2024-12-11 09:36:07 +0800

    chore: slot-node

--- src/components/slot-node/slot-node.interface.ts
+++
@@ -1,7 +0,0 @@
-import { TypeNode } from '../../core/type-node/type-node.abstract';
-import { ITypeConfig } from '../../core/type-node/type-node.interface';
-
-export interface ISlotNodeConfig extends ITypeConfig {
-  name: string; // default .....
-  slot?: string | TypeNode | (string | TypeNode)[]
-}
