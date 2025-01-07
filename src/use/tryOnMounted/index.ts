d171226c0baf596d27bc8e27f5efca70d3823de6
jeffrey xu <jianfeng.xu@cssca.com>
2025-01-07 09:36:06 +0800

    chore: tryOnMounted

---
+++ src/use/tryOnMounted/index.ts
@@ -0,0 +1,23 @@
+import { onMounted } from '../../core/apiLifecycle'
+import { nextTick } from '../../util/next-tick'
+import type { Fn } from '../utils'
+// eslint-disable-next-line no-restricted-imports
+// import { nextTick, onMounted } from 'vue'
+import { getLifeCycleTarget } from '../utils'
+
+/**
+ * Call onMounted() if it's inside a component lifecycle, if not, just call the function
+ *
+ * @param fn
+ * @param sync if set to false, it will run in the nextTick() of Vue
+ * @param target
+ */
+export function tryOnMounted(fn: Fn, sync = true, target?: any) {
+  const instance = getLifeCycleTarget()
+  if (instance)
+    onMounted(fn, target)
+  else if (sync)
+    fn()
+  else
+    nextTick(fn)
+}
