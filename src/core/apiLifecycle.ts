4843c3a42c6ee5a2eda8590625e54b5cb558c978
jeffrey xu <jianfeng.xu@cssca.com>
2025-01-13 14:16:06 +0800

    chore: apiLifecycle

---
+++ src/core/apiLifecycle.ts
@@ -0,0 +1,109 @@
+import { AnyFn } from '../interface';
+import { LifecycleHooks } from './enums';
+import { currentInstance, setCurrentInstance } from './instance';
+import { TypeNode } from './type-node/type-node.abstract';
+import { DebuggerEvent } from '../debug';
+import { warn } from '../util/debug';
+import { toHandlerKey } from '../shared/general';
+import { ErrorTypeStrings } from './errorHandling';
+
+export function injectHook(
+  type: LifecycleHooks,
+  hook: AnyFn,
+  target: TypeNode | null = currentInstance,
+  prepend = false
+): AnyFn | undefined {
+  if (target) {
+    const hooks = target.lifeCycles[type] || (target.lifeCycles[type] = []);
+    // cache the error handling wrapper for injected hooks so the same hook
+    // can be properly deduped by the scheduler. "__weh" stands for "with error
+    // handling".
+    const wrappedHook = hook;
+    // hook.__weh ||
+    // (hook.__weh = (...args: unknown[]) => {
+    //   // disable tracking inside all lifecycle hooks
+    //   // since they can potentially be called inside effects.
+    //   pauseTracking()
+    //   // Set currentInstance during hook invocation.
+    //   // This assumes the hook does not synchronously trigger other hooks, which
+    //   // can only be false when the user does something really funky.
+    //   const reset = setCurrentInstance(target)
+    //   const res = callWithAsyncErrorHandling(hook, target, type, args)
+    //   reset()
+    //   resetTracking()
+    //   return res
+    // })
+    if (prepend) {
+      hooks.unshift(wrappedHook);
+    } else {
+      hooks.push(wrappedHook);
+    }
+    return wrappedHook;
+  }
+  // else if (__DEV__) {
+  //   const apiName = toHandlerKey(ErrorTypeStrings[type].replace(/ hook$/, ''));
+  //   warn(
+  //     `${apiName} is called when there is no active component instance to be ` +
+  //     `associated with. ` +
+  //     `Lifecycle injection APIs can only be used during execution of setup().`
+  //     // +
+  //     //   (__FEATURE_SUSPENSE__
+  //     //     ? ` If you are using async setup(), make sure to register lifecycle ` +
+  //     //       `hooks before the first await statement.`
+  //     //     : ``),
+  //   );
+  // }
+  return undefined;
+}
+
+const createHook =
+  <T extends AnyFn = () => any>(lifecycle: LifecycleHooks) =>
+    (
+      hook: T,
+      target: TypeNode | null = currentInstance
+    ): void => {
+      // post-create lifecycle registrations are noops during SSR (except for serverPrefetch)
+      // if (
+      //   // !isInSSRComponentSetup ||
+      //   lifecycle === LifecycleHooks.SERVER_PREFETCH
+      // ) {
+      injectHook(lifecycle, (...args: unknown[]) => hook(...args), target);
+      // }
+    };
+type CreateHook<T = any> = (
+  hook: T,
+  target?: TypeNode | null
+) => void
+
+export const onBeforeMount: CreateHook = createHook(LifecycleHooks.BEFORE_MOUNT);
+export const onMounted: CreateHook = createHook(LifecycleHooks.MOUNTED);
+export const onBeforeUpdate: CreateHook = createHook(
+  LifecycleHooks.BEFORE_UPDATE
+);
+export const onUpdated: CreateHook = createHook(LifecycleHooks.UPDATED);
+export const onBeforeUnmount: CreateHook = createHook(
+  LifecycleHooks.BEFORE_UNMOUNT
+);
+export const onUnmounted: CreateHook = createHook(LifecycleHooks.UNMOUNTED);
+export const onServerPrefetch: CreateHook = createHook(
+  LifecycleHooks.SERVER_PREFETCH
+);
+
+export type DebuggerHook = (e: DebuggerEvent) => void
+export const onRenderTriggered: CreateHook<DebuggerHook> =
+  createHook<DebuggerHook>(LifecycleHooks.RENDER_TRIGGERED);
+export const onRenderTracked: CreateHook<DebuggerHook> =
+  createHook<DebuggerHook>(LifecycleHooks.RENDER_TRACKED);
+
+export type ErrorCapturedHook<TError = unknown> = (
+  err: TError,
+  instance: TypeNode | null,
+  info: string
+) => boolean | void
+
+export function onErrorCaptured<TError = Error>(
+  hook: ErrorCapturedHook<TError>,
+  target: TypeNode | null = currentInstance
+): void {
+  injectHook(LifecycleHooks.ERROR_CAPTURED, hook, target);
+}
