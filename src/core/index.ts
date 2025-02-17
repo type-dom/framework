e8710210b7b0d1f030b081e10de609250ec31de9
jeffrey xu <jianfeng.xu@cssca.com>
2025-02-17 09:51:08 +0800

    chore: core

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,16 +1,22 @@
 export * from './type-html';
 
+export * from './x-proxy/x-proxy.class';
+export type * from './x-proxy/x-proxy.interface';
 export type * from './event-emitter/event-emitter.interface';
 export { SlotNode } from './slot-node/slot-node.class';
 export { TypeFragment } from './type-fragment/type-fragment.abstract';
-export type { ITypeFragment } from './type-fragment/type-fragment.interface';
+export type * from './type-fragment/type-fragment.interface';
 export { vHash, TypeElement } from './type-element/type-element.abstract';
 export type * from './type-element/type-element.interface';
 export * from './style/index';
 
 export { TypeSvg } from './type-svg/type-svg.abstract';
-export type { ITypeSvg } from './type-svg/type-svg.interface';
+export type * from './type-svg/type-svg.interface';
 export { TypeSvgSvg } from './type-svg/svg/svg.abstract';
-export type { ITypeSvgSvg } from './type-svg/svg/svg.interface';
+export type * from './type-svg/svg/svg.interface';
 
 export * from './util';
+export * from './instance';
+export * from './apiInject';
+export * from './apiLifecycle';
+export * from './apiSetupHelpers';
