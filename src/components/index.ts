a9fda1be4a4bd5dbd940b5e1d0d0be56ace5daac
jeffrey xu <jianfeng.xu@cssca.com>
2025-02-20 09:42:19 +0800

    chore components

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,8 @@ export type * from './transition/transition.interface';
 export * from './transition/transition.util';
 export { XElement } from './x-element/x-element.class';
 export type { IXElement } from './x-element/x-element.interface';
+export { Fragment } from './fragment/fragment.class';
+export type * from './fragment/fragment.interface';
 /**
  * 这里是TypeDom框架的基础组件
  */
