61def1213e08fc74a47f819b8c22594d2e1f0c86
jeffrey xu <jianfeng.xu@cssca.com>
2025-02-25 09:28:03 +0800

    chore: useWindowFocus

---
+++ src/use/useWindowFocus/component.ts
@@ -0,0 +1,16 @@
+// import { useWindowFocus } from '@vueuse/core'
+// import { defineComponent, reactive } from 'vue'
+//
+// export const UseWindowFocus = /* #__PURE__ */ defineComponent({
+//   name: 'UseWindowFocus',
+//   setup(props, { slots }) {
+//     const data = reactive({
+//       focused: useWindowFocus(),
+//     })
+//
+//     return () => {
+//       if (slots.default)
+//         return slots.default(data)
+//     }
+//   },
+// })
---
+++ src/use/useWindowFocus/demo.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useWindowFocus } from '@vueuse/core'
+import { ref, watch } from 'vue'
+
+const startMessage = '💡 Click somewhere outside of the document to unfocus.'
+const message = ref(startMessage)
+const focused = useWindowFocus()
+
+watch(focused, (isFocused) => {
+  if (isFocused)
+    message.value = startMessage
+  else
+    message.value = 'ℹ Tab is unfocused'
+})
+</script>
+
+<template>
+  <div>{{ message }}</div>
+</template>
