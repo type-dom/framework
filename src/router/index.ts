9158708c831ab43354e1d6e4593403f9fb1e35ef
jeffrey xu <jianfeng.xu@cssca.com>
2025-03-31 09:53:42 +0800

    chore: router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,3 +2,4 @@ export { Router } from './router.class';
 export type * from './router.interface';
 export { RouterView } from './router-view/router-view.class';
 export type { IRoute } from './route.interface';
+export * from './util';
