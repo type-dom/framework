001c3e9644c6ad5038a064b0586d2cd3e9d5f13f
jeffrey xu <jianfeng.xu@cssca.com>
2025-04-03 17:53:12 +0800

    chore: useScroll

--- src/use/useScroll/directive.test.ts
+++ src/use/useScroll/directive.test.ts
@@ -1,6 +1,6 @@
-import type { VueWrapper } from '@vue/test-utils'
+import type { VueWrapper } from '@vue/tests-utils'
 import type { UseScrollOptions } from '.'
-import { mount } from '@vue/test-utils'
+import { mount } from '@vue/tests-utils'
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { defineComponent } from 'vue'
 import { vScroll } from './directive'
