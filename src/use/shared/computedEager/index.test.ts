450e1061bb290707024385ceb66550b5638422f1
jeffrey xu <jianfeng.xu@cssca.com>
2025-04-17 09:33:09 +0800

    chore: computedEager

--- src/use/shared/computedEager/index.test.ts
+++ src/use/shared/computedEager/index.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it, vi } from 'vitest'
 import { computed, ref, watch } from 'vue'
 import { computedEager } from '.'
-import { nextTwoTick } from '../../.test'
+import { nextTwoTick } from '../../.tests'
 
 describe('computedEager', () => {
   it('should be defined', () => {
