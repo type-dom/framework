3322fbcf07162e4059f41b73ced7beab2e589aa9
jeffrey xu <jianfeng.xu@cssca.com>
2025-05-06 15:41:49 +0800

    chore: utils

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -27,7 +27,7 @@ if (inBrowser) {
         supportsPassive = true;
       }
     } as object); // https://github.com/facebook/flow/issues/285
-    window.addEventListener('tests-passive', null as any, opts);
+    window.addEventListener('test-dts-passive', null as any, opts);
   } catch (e: any) {
     /* empty */
   }
