483147eaf07e3e83060fbbf98b58ff5d43bc398f
jeffrey xu <jianfeng.xu@cssca.com>
2025-05-06 15:42:15 +0800

    chore: test

---
+++ src/use/.test/index.ts
@@ -0,0 +1,6 @@
+// import process from 'node:process'
+//
+// export const isBelowNode18 = Number(process.version.slice(1).split('.')[0]) < 18
+
+// export * from './mount'
+export * from './nextTick'
