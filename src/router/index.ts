ea287f0820d547c47363890fc9603556d7a1035e
jeffrey xu <jianfeng.xu@cssca.com>
2025-05-07 09:37:51 +0800

    chore: delete router

--- src/router/index.ts
+++
@@ -1,5 +0,0 @@
-export { Router } from './router.class';
-export type * from './router.interface';
-export { RouterView } from './router-view/router-view.class';
-export type { IRoute } from './route.interface';
-export * from './util';
