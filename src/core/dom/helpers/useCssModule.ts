bfc2bc3342f715d2661f3b91bbbc7ec7e875eb55
jeffrey xu <jianfeng.xu@cssca.com>
2025-07-15 11:05:17 +0800

    chore: add dom

---
+++ src/core/dom/helpers/useCssModule.ts
@@ -0,0 +1,32 @@
+// // import { getCurrentInstance, warn } from '@vue/runtime-core'
+// import { getCurrentInstance } from 'src/core/instance';
+// import { EMPTY_OBJ } from '../../../shared'
+// import { warn } from '../../../utils';
+//
+// export function useCssModule(name = '$style'): Record<string, string> {
+//   if (!__GLOBAL__) {
+//     const instance = getCurrentInstance()!
+//     if (!instance) {
+//       warn(`useCssModule must be called inside setup()`)
+//       return EMPTY_OBJ
+//     }
+//     const modules = instance.type.__cssModules
+//     if (!modules) {
+//       warn(`Current instance does not have CSS modules injected.`)
+//       return EMPTY_OBJ
+//     }
+//     const mod = modules[name]
+//     if (!mod) {
+//         warn(`Current instance does not have CSS module named "${name}".`)
+//       return EMPTY_OBJ
+//     }
+//     return mod as Record<string, string>
+//   } else {
+//     /* v8 ignore start */
+//     // if (__DEV__) {
+//       warn(`useCssModule() is not supported in the global build.`)
+//     // }
+//     return EMPTY_OBJ
+//     /* v8 ignore stop */
+//   }
+// }
