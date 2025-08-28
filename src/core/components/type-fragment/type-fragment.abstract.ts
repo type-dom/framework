b14de98050d80eafeee167ab6be1323d6553957a
jeffrey xu <jianfeng.xu@cssca.com>
2025-08-28 14:43:55 +0800

    chore: type-fragment

--- src/core/components/type-fragment/type-fragment.abstract.ts
+++ src/core/components/type-fragment/type-fragment.abstract.ts
@@ -6,6 +6,7 @@ import { TypeProps } from '../../type-node/type-node.interface';
 import { TypeElement } from '../../type-element/type-element.abstract';
 import { NodeName } from '../../enums';
 import { onBeforeMount } from '../../apiLifecycle';
+import { assignProps } from '../../helpers/assignProps';
 import { ITypeFragment, TypeFragmentProps } from './type-fragment.interface';
 
 /**
@@ -20,9 +21,13 @@ export abstract class TypeFragment extends TypeElement implements ITypeFragment 
 
   constructor() {
     super();
-    this.props = this.useParams({
+    // this.props = this.useParams({
+    //   nodeName: NodeName.FRAGMENT,
+    // })
+    assignProps(this, {
       nodeName: NodeName.FRAGMENT,
-    })
+    });
+    this.props = this.baseProps as TypeFragmentProps;
   }
 
   addStyleObj(styleObj?: StyleValue) {
@@ -74,9 +79,19 @@ export abstract class TypeFragment extends TypeElement implements ITypeFragment 
   // 向下传递 styleObj attrObj;
   override useParams<Props extends TypeProps>(params = {} as Props): Props {
     super.useParams(params);
+    // todo mount 时， vIf为 false 时，要添加 this.anchor
+    // if (this.className === undefined) {
+    //   console.error('element.className === undefined , element is ', this);
+    // }
+    // if (this.uid === 50) {
+    //   console.error('element.uid === 50 , element is ', this);
+    // }
+    // this.anchor = document.createComment('fragment-' + this.className);
+    this.anchorStart = this.anchorStart ?? document.createComment('[--' + this.className + '' + this.uid);
+    this.anchor = this.anchor ?? document.createComment(this.className + '' + this.uid + '--]');
     // 子元素可能是 setup中新增的，这时 this.childNodes可能没有或不全；
-    onBeforeMount(() => {
-      this.anchor = document.createComment('fragment-' + this.className);
+    //   todo 渲染时，判断一下，先添加parent.styleObj
+    onBeforeMount(() => { // 还是有可能 childNodes 没有加全
       this.childNodes.forEach(child => {
         if (child instanceof TypeFragment) {
           child.addStyleObj(params.styleObj);
