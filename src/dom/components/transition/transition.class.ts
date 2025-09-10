f083ee86f920116148b810f9cb199739dc3b3b41
jeffrey xu <jianfeng.xu@cssca.com>
2025-09-10 09:42:01 +0800

    chore: transition

--- src/dom/components/transition/transition.class.ts
+++ src/dom/components/transition/transition.class.ts
@@ -1,5 +1,6 @@
 import { TypeTransition } from '../../../core/components/type-transition/type-transition.abstract';
 import { TypeTransitionProps } from '../../../core/components/type-transition/type-transition.interface';
+// import { transformSlot } from '../../../core/helpers/transformSlot';
 import type { ITransition, TransitionProps } from './transition.interface';
 import { resolveTransitionProps } from './transition.util';
 
@@ -8,8 +9,10 @@ export class Transition extends TypeTransition implements ITransition {
   override props: TypeTransitionProps<Element>;
 
   constructor(params: TransitionProps = {}) {
-    super();
+    super(params);
     this.className = 'Transition';
+    // todo 在 TypeTransition的 setup 中有添加；
+    // transformSlot(this, params.slot); // childNodes parent // 会有2个 content
     this.useParams(params);
     const props = resolveTransitionProps(params);
     this.props = this.useParams(props); // dom 操作 nodeName = fragment
